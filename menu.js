586ee4bc030f17408ed0a1dd235d01d847911e10
gustavoPassone <gmpassone@gmail.com>
2025-11-01 11:09:19 -0300

    finalizado o menu do cadastro com POO

--- menu.js
+++ menu.js
@@ -25,7 +25,9 @@ class Pessoa {
 
   // metodo especial que permite eu gerenciar a visualização da senha
   get senha() {
-    const palavraChave = prompt("Para ver sua senha, você precisa falar a palavra chave: ");
+    const palavraChave = prompt(
+      "Para ver sua senha, você precisa falar a palavra chave: "
+    );
 
     if (this.palavraChave == palavraChave) {
       console.log(`Você acertou a palavra chave. Sua senha é: ${this.#senha}`);
@@ -35,25 +37,102 @@ class Pessoa {
   }
 }
 
+// herança - extends
+class Aluno extends Pessoa {
+  constructor(nome, dataNasc, senha, RA) {
+    super(nome, dataNasc, senha);
+    this.RA = RA;
+  }
+
+  // polimorfismo
+  // sobrescrita - override
+  apresentar() {
+    console.log(`Olá aluno ${this.nome}`);
+    console.log("-- Essas são suas informações -- ");    
+    console.log(`• RA: ${this.RA}`);
+  }
+}
+
+class Professor extends Pessoa {
+  // os parametros são os valores das chaves(propriedades)
+  constructor(nome, dataNasc, senha, salario, registro) {
+    // super - puxar as propriedades da classe Pessoa
+    super(nome, dataNasc, senha);
+    // são as chaves = propriedades da classe
+    this.salario = salario;
+    this.registro = registro;
+
+  }
+  
+  apresentar() {
+    console.log(`Olá professor ${this.nome}`);
+    console.log("--Essas são suas informações--");
+    console.log(`• Salario: ${this.salario}`);
+    console.log(`• Registro: ${this.registro}`);
+  }
+}
+
+console.log("-- Bem vindo ao cadastro👋 --");
+console.log("Se for PROFESSOR digite 1");
+console.log("Se for ALUNO digite 2");
+let professorOuAluno = Number(prompt(""));
+
+let novaPessoa;
+
 let nomePessoa = prompt("Digite seu nome: ");
-let idadePessoa = Number(prompt("Digite sua idade: "));
-let dataNascPessoa = prompt("Digite sua data de nascimento - formato ano/mês/dia: ");
+let dataNascPessoa = prompt(
+  "Digite sua data de nascimento - formato ano/mês/dia: "
+);
 let senhaPessoa = prompt("Digite sua senha: ");
-let palavraChavePessoa = prompt("Digite sua palavra chave: ");
+
+// verificar se é aluno ou professor
+if (professorOuAluno == 1) {
+  let registroProfessor = prompt("Digite seu registro:");
+  let salarioProfessor = prompt("Digite seu salario:");
+
+  // instanciando classe Professor
+  novaPessoa = new Professor(
+    nomePessoa, 
+    dataNascPessoa,
+    senhaPessoa,
+    salarioProfessor,
+    registroProfessor
+  )
+
+} else if (professorOuAluno == 2) {
+  let raAluno = prompt("Digite seu RA:");
+
+  // instanciando classe Aluno
+  novaPessoa = new Aluno(
+    nomePessoa, 
+    dataNascPessoa,
+    senhaPessoa,
+    raAluno
+  )
+
+} else {
+  console.log("Opção inválida. Tente novamente");
+}
+
+
+console.log(novaPessoa);
+console.log(novaPessoa.apresentar());
+
 
 // criando o objeto / instância
-const novaPessoa = new Pessoa(
+/* const novaPessoa = new Pessoa(
   nomePessoa,
   idadePessoa,
   dataNascPessoa,
   senhaPessoa,
   palavraChavePessoa
-);
-
+); */
 
 // aciona a função do objeto já criado
-novaPessoa.apresentar();
-novaPessoa.senha
+// novaPessoa.apresentar();
+// novaPessoa.senha;
+
+// verificarPessoa();
 
 // exibir todo o objeto
 // console.log(novaPessoa);
